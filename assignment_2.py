8b58e8f8f28581f68323a4c0c0975d5f73cf3f4a
samaanaa <samaneh.namaki@gmail.com>
2019-05-05 17:12:12 +0430

    assignment_2

--- assignment_2.py
+++ assignment_2.py
@@ -1,11 +1,12 @@
-data=[]
 
-data=input("input data list")
-for i in data[i]
+data=[50.3, 338.4, 626.5, 959.4, 1317.9, 1716.7, 2134.3, 2565.5, 3085.6, 3626.7]
+
+duration=[]
+i=0
+
+for i in range(len(data)):
 duration[i]=data[i+1]-data[i]
 
 print(duration)
 
 
-for in in lst for datum in if :
-    
---
+++ assignment_2_2.py
@@ -0,0 +1,16 @@
+
+
+T_1=300
+T_2=400
+my_str=
+
+for i in range(duration)
+
+    if duration[i]<T_1:
+      append.my_str(S)
+
+
+        if duration[i] >T_2:
+            append.my_str(S)
+            if  T_1< duration[i] < T_2:
+                append.my_str(S)
