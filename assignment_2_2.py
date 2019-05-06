b94b5978caf5640a3f50085d382ace3517ebf904
samaanaa <samaneh.namaki@gmail.com>
2019-05-06 16:20:45 +0430

    part_2_Q1

--- assignment_2_2.py
+++ assignment_2_2.py
@@ -9,8 +9,7 @@ for i in range(duration)
     if duration[i]<T_1:
       append.my_str(S)
 
-
         if duration[i] >T_2:
-            append.my_str(S)
+            append.my_str(L)
             if  T_1< duration[i] < T_2:
-                append.my_str(S)
+                append.my_str(M)
