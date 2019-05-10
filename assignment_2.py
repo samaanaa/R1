6dd1921a145d3f909706a6f99acdee8a91a780ae
samaanaa <samaneh.namaki@gmail.com>
2019-05-10 19:27:05 +0430

    assignment2

--- assignment_2.py
+++ assignment_2.py
@@ -1,12 +1,9 @@
+data = [50.3, 338.4, 626.5, 959.4, 1317.9, 1716.7, 2134.3, 2565.5, 3085.6, 3626.7]
 
-data=[50.3, 338.4, 626.5, 959.4, 1317.9, 1716.7, 2134.3, 2565.5, 3085.6, 3626.7]
+duration = []
+i = 0
 
-duration=[]
-i=0
-
-for i in range(len(data)):
-duration[i]=data[i+1]-data[i]
+duration = [y - x for x, y in zip(data[:-1], data[1:])]
 
 print(duration)
-
 
--- assignment_2_2.py
+++ assignment_2_2.py
@@ -1,15 +1,16 @@
+duration=[288.09999999999997, 288.1, 332.9, 358.5000000000001, 398.79999999999995, 417.60000000000014, 431.1999999999998,
+ 520.0999999999999, 541.0999999999999]
 
+T_1 = 300
+T_2 = 400
 
-T_1=300
-T_2=400
-my_str=
+for i in duration:
 
-for i in range(duration)
+    if duration[i] < T_1:
+        append.my_str(S)
 
-    if duration[i]<T_1:
-      append.my_str(S)
-
-        if duration[i] >T_2:
+        if duration[i] > T_2:
             append.my_str(L)
-            if  T_1< duration[i] < T_2:
+
+            if T_1 < duration[i] < T_2:
                 append.my_str(M)
